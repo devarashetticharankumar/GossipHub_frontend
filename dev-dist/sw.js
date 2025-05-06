aae161a6ee68ce12b87b05cf1f41b4a01b547d4b
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2025-05-06 20:02:12 +0530

    2nd commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v7o8g3u4ejo"
+    "revision": "0.6j243v7fjc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
